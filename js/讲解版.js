73e93b345ec57b9da952693ba8de5ae731069032
南小街 <396967872@qq.com>
2018-08-23 10:53:23 +0800

    自运行函数

--- js/讲解版.js
+++ js/讲解版.js
@@ -1,57 +1,71 @@
-// 规定每张图片处于的位置和状态
-var states=[
-    {ZIndex:1,width:120,height:150,top:69,left:134,opacity:0.2},
-    {ZIndex:2,width:130,height:170,top:59,left:0,opacity:0.5},
-    {ZIndex:3,width:170,height:218,top:24,left:110,opacity:0.7},
-    {ZIndex:4,width:224,height:288,top:0,left:263,opacity:1},
-    {ZIndex:3,width:170,height:218,top:24,left:470,opacity:0.7},
-    {ZIndex:2,width:130,height:170,top:59,left:620,opacity:0.5},
-    {ZIndex:1,width:120,height:150,top:69,left:500,opacity:0.2},
-]
-// 将状态 和位置赋值给li标签
-var lis=$('#box li');
-function move(){
-    lis.each(function(index,ele){
-        var state=states[index]
-        $(ele).css('z-index',state.ZIndex).finish().animate(state,1000).find('img').css('opacity',state.opacity)
-    })
-}
-move()
-
-function next(){
-    states.unshift(states.pop())
-    move();
-}
-next()
-
-// 下一张
-    $('.next').click(function(){
-    // 原理：把数组中的最后一个元素移动到数组中的第一个
-    // states.unshift(states.pop())
-    // move();
-    next()
-})
-    $('.prev').click(function(){
-    // 原理：把数组的第一个元素移动到最后一位
-    states.push(states.shift())
+// 首先可以将整体封装成一个匿名的自运行函数
+(function(){
+    // 规定每张图片处于的位置和状态
+    var states=[
+        {ZIndex:1,width:120,height:150,top:69,left:134,opacity:0.2},
+        {ZIndex:2,width:130,height:170,top:59,left:0,opacity:0.5},
+        {ZIndex:3,width:170,height:218,top:24,left:110,opacity:0.7},
+        {ZIndex:4,width:224,height:288,top:0,left:263,opacity:1},
+        {ZIndex:3,width:170,height:218,top:24,left:470,opacity:0.7},
+        {ZIndex:2,width:130,height:170,top:59,left:620,opacity:0.5},
+        {ZIndex:1,width:120,height:150,top:69,left:500,opacity:0.2},
+    ]
+    // 将状态 和位置赋值给li标签
+    var lis=$('#box li');
+    function move(){
+        lis.each(function(index,ele){
+            var state=states[index]
+            $(ele).css('z-index',state.ZIndex).finish().animate(state,1000).find('img').css('opacity',state.opacity)
+        })
+    }
     move()
-})
 
-// 自动轮播
-var time=null
-function autoPlay(){
-    time=setInterval(function(){
+    function next(){
+        states.unshift(states.pop())
+        move();
+    }
+    next()
+
+    // 下一张
+        $('.next').click(function(){
+        // 原理：把数组中的最后一个元素移动到数组中的第一个
+        // states.unshift(states.pop())
+        // move();
         next()
-    },3000)
-}
-autoPlay()
+    })
+        $('.prev').click(function(){
+        // 原理：把数组的第一个元素移动到最后一位
+        states.push(states.shift())
+        move()
+    })
 
-// 停止轮播
-$('#box section').add('#box li').hover(function(){
-    clearInterval(time)
-},function(){
+    // 自动轮播
+    var time=null
+    function autoPlay(){
+        time=setInterval(function(){
+            next()
+        },3000)
+    }
     autoPlay()
-})
+
+    // 停止轮播
+    $('#box section').add('#box li').hover(function(){
+        clearInterval(time)
+    },function(){
+        autoPlay()
+    })
+})()
+// 变量的作用域问题:
+    // 1.全局域[window]  2.函数域[function]
+    // 1.全局域:从页面被打开之后到页面关闭之前,始终都是存在的
+    // 2.函数域:存在函数被调用的一瞬间,(也不一定,考虑闭包的存在)
+        // 闭包的作用:除了连接以外,可以保留函数的作用域(所以move可以使用当前自运行函数中的states)，
+        // 闭包产生的必要条件：函数里面包函数(内层的函数要使用外层函数的变量)
+
+        // 全局变量会产生闭包？
+        // 不会，因为全局变量存在全局域中
+
+
 
 // 封装为插件
 // 能够使得只要使用这个插件，就能被重复的使用效果，会产生什么问题？
